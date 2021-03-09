6e0524750328d40c091fa2187b0e0de466c38898
etiwitty <atiyeh.keshavarz@gmail.com>
2021-03-08 17:58:35 -0800

    implemented promise

--- callbacks.js
+++ callbacks.js
@@ -52,3 +52,36 @@ function createPost(post, callback) {
 
 createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
 // So now it waited 2 sec and then show all of the post - still we can improve the delay in next solution
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Solution 2 - promises ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function getPosts() {
+  console.log("we are here");
+  setTimeout(()=> {
+    let output = '';
+    posts.forEach((post, index)=> {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      // to resolve
+      const error = false;
+      if(!error) {
+        resolve();
+      } else {
+        reject('Error: Something went wrong!')
+      }
+    }, 2000);
+  })
+}
+
+
+createPost({title: 'Post Three', body: 'This is post three'})
+  .then(getPosts)
+  .catch(err => console.log(err));
