7295bc849d5f3daf6945bd62c9b824fd9f84e150
etiwitty <atiyeh.keshavarz@gmail.com>
2021-03-08 17:04:45 -0800

    understand the callback function necessite

--- callbacks.js
+++ callbacks.js
@@ -14,5 +14,17 @@ function getPosts() {
   }, 1000);
 }
 
+//hypothetically we are working with server - so we use setTimeOut to mimic
+function createPost(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+
 getPosts();
 
+createPost({title: 'Post Three', body: 'This is post three'})
+
+// we are not seeing post 3 - the reason is beacuse the createPost took longer than getPost. By the time
+// it create post, DOM is already painted. It can't do anything beyond that point  this is where async programming comes in, and this is 
+// when callbacks come in, which is one way to handle it 
